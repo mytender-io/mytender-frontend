f5d7c4c4c3063b89884cbac0b596eb5013e30496
Nicolas Dickreuter <dickreuter@gmail.com>
2025-01-28 20:28:41 +0000

    fix wrong case

--- src/routes/BreadCrumbNavigation.tsx
+++ src/routes/BreadCrumbNavigation.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ChevronRight, Home } from 'lucide-react';
-import './BreadcrumbNavigation.css';
+import './BreadCrumbNavigation.css';
 
 const BreadcrumbNavigation = ({
   currentPage,
