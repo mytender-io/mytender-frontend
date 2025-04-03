5b5a2ee41e6c7d599969e6c01c902cf13cb55f5c
rgnatenko <rgnatenko22@gmail.com>
2025-04-03 11:24:02 +0300

    exclude 4merit1 from tracking in posthog

--- src/constants/posthogUsers.ts
+++ src/constants/posthogUsers.ts
@@ -1,1 +1,1 @@
-export const USERS_TO_EXCLUDE_IN_POSTHOG = ["raff", "jamierag", "mytendercore", "adminuser"];
+export const USERS_TO_EXCLUDE_IN_POSTHOG = ["raff", "jamierag", "mytendercore", "adminuser", "4merit1"];
