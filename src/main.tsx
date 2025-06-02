035ed871b4b233cabf0e1d2dda9a4951913e876d
ibm-work <kevinsofyan528@gmail.com>
2025-06-02 14:28:26 -0400

    upgrade versions for react and mdxeditor

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
