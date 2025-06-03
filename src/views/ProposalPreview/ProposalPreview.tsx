73fbdccaed5effaede2eb8700d4bc51cd323d357
marghoobtarar0344 <marghoobahmad0344@gmail.com>
2025-06-03 12:33:20 +0500

    updating body parameters

--- src/views/ProposalPreview/ProposalPreview.tsx
+++ src/views/ProposalPreview/ProposalPreview.tsx
@@ -1056,6 +1056,7 @@ const ProposalPreview = ({ activeSectionId }: { activeSectionId: string }) => {
                     {/* Selection Menu */}
                     {showSelectionMenu && (
                       <TextSelectionMenu
+                        question={outline[currentSectionIndex].question}
                         selectionMenuPosition={selectionMenuPosition}
                         selectedRange={selectedRange}
                         setSelectedRange={setSelectedRange}
